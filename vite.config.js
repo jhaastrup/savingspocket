4e4fc35884f5460f1224233da6797923c227bd26
jhaastrup <jhaastrup21@gmail.com>
2025-07-16 14:34:06 +0100

    fixing deployment issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // Correct import for v4
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),] // Correct usage as a Vite plugin
+  plugins: [react(), tailwindcss(),],// Correct usage as a Vite plugin
+  build: {
+    target: 'esnext', // <--- Add this line
+  },
+  base: './',
 })
 
